afc7534398c8feeb0001803c3358e81c450b18b3
Marie Machalova <xplevk00@vutbr.cz>
2021-05-03 10:37:21 +0200

    ukol1

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def recursive_nth_fibo(n):
+    if n < 2:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    n = 6
+    output = recursive_nth_fibo(n)
+    print(output)
 
 
 if __name__ == '__main__':
